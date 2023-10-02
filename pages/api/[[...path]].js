e2ecd79addd714596d922a8319f934c8b45185de
xpollen8 <github@xpollen8.com>
2023-10-01 20:32:45 -0700

    dw: presses_for_admin

--- pages/api/[[...path]].js
+++ pages/api/[[...path]].js
@@ -14,6 +14,7 @@ const queries = [
 	{ noun: "gigs_by_musician", key: "p.performer", query: "select * from performance p, gig g where {{key}} like '[[person:{{value}}%' and p.datetime=g.datetime" },
 	{ noun: "gigs_by_song", key: "s.song", query: "select * from gigsong s, gig g where {{key}} like '%{{value}}%' and s.datetime=g.datetime" },
 	{ noun: "presses", query: "select url, type, person, dtadded, dtpublished, dtgig, todo, album, thumb, images, media, publication, location, title, headline, subhead, summary, source, credit from press" },
+	{ noun: "presses_for_admin", query: "select * from press" },
 	{ noun: "medias", query: "select * from media" },
 	{ noun: "feedbacks", query: "select * from feedback where domain_id=11" },
 	{ noun: "feedback", query: "select * from feedback where domain_id=11 and uri like '{{value}}%' order by dtcreated desc" },
