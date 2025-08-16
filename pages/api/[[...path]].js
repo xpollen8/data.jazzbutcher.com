e744782dbda1ce7df5866f9bcf605e552faeb6fa
xpollen8 <github@xpollen8.com>
2025-08-15 20:19:12 -0700

    dw: null removal

--- pages/api/[[...path]].js
+++ pages/api/[[...path]].js
@@ -129,6 +129,7 @@ const doQuery = async (noun, key, type, value) => {
 		const unUTC = (timestampStr) => {
 			try {
 				return new Date(new Date(timestampStr)?.getTime() - (new Date(timestampStr)?.getTimezoneOffset() * 60 * 1000))?.toISOString()?.replace(/T/, ' ')?.replace(/Z/, '')?.substr(0, 19);
+				//return new Date(new Date(timestampStr)?.getTime() - (new Date(timestampStr)?.getTimezoneOffset() * 60 * 1000))?.toISOString()?.replace(/T/, ' ')?.replace(/Z/, '')?.substr(0, 19)?.replace(/ 00:00:00/, '');
 			} catch (e) {
 				// return as-is
 				return timestampStr;
@@ -139,7 +140,7 @@ const doQuery = async (noun, key, type, value) => {
 			// remove empty attributes from data structure
 			Object.keys(row).forEach(index => {
 				// keep explicit '0' values
-				if ((!row[index] && row[index] !== 0) || row[index] === '0000-00-00 00:00:00') {
+				if (row[index] === null || (!row[index] && row[index] !== 0) || row[index] === '0000-00-00 00:00:00') {
 					delete row[index];
 				} else if (index === 'added' || index === 'datetime' || index === 'dtadded' || index === 'dtgig' || index === 'dtpublished' || index === 'credit_date' || index === 'dtcreated') {
 					row[index] = unUTC(row[index]);
